d30a487cbbe685d6a3847860265d89ccea4cef7c
Richi <ricardorg20@gmail.com>
2021-03-11 10:23:30 +0100

    Estructura pagina

--- src/bluerenting-main/bluerenting-main.js
+++ src/bluerenting-main/bluerenting-main.js
@@ -4,22 +4,52 @@ class BluerentingMain extends LitElement {
 
     static get properties() {
         return {
+            coches: {type: Array},
         }
     }
 
     constructor() {
         super()
 
-        this.title = 'Blue Renting';
+        this.coches = [];
+        this.getCoches()
     }
 
     render() {
         return html`
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-        <h1>Main Page</h1>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+            <h1>Main Page</h1>
+            <h1>Coches: ${this.coches}</h1>
+
+
         `;
     }       
 
+    getCoches() {
+        console.log("Get Coches")
+        // Usamos AJAX
+        let xhr = new XMLHttpRequest();
+
+        // onload signifcica que se ha lanzado la petición y ha llegado, en este caso lo que estoy haciendo es 
+        // cuando se finaliza la petición se ejecuta la función anonima ()
+        // Funcion anonima que no tiene nombre, por eso se pone () nada más, podria ser ... = hola() => {...}
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText)
+                let APIResponse = JSON.parse(xhr.responseText);
+
+                // El campo que nos interesa de esa api se llama results
+                this.coches = APIResponse.results
+                console.log(APIResponse)
+            }
+        }
+
+        // open no envia la petición, es como una especia de init
+        xhr.open("GET", "https://dev-bluerenting-dluj3qlvta-oa.a.run.app/api/v2/vehicles");
+        // Hacemos la petición
+        xhr.send();
+
+    }
 
 }
 
--- src/bluerenting-menu/bluerenting-menu.js
+++ src/bluerenting-menu/bluerenting-menu.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html , css} from 'lit-element';
 
 class BluerentingMenu extends LitElement {
 
@@ -13,10 +13,18 @@ class BluerentingMenu extends LitElement {
         this.title = 'Blue Renting';
     }
 
+    static get styles() {
+        return css`
+            .navbar-custom { 
+                background-color: #060A4B; 
+            }
+        `;
+      }
+
     render() {
         return html`
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
             <a class="navbar-brand" href="/">Blue Renting</a>
 
             <div class="collapse navbar-collapse" id="navbar-bluerenting">
