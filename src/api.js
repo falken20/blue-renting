16845007dcacc58129bb233c5c94a664efa6aa00
Richi <ricardorg20@gmail.com>
2021-03-12 10:51:18 +0100

    Arreglos Cesta

--- src/api.js
+++ src/api.js
@@ -1,2 +1,3 @@
 export const apiVehicles = "https://bluerenting-dluj3qlvta-oa.a.run.app/api/v2/vehicles"
 export const apiUsers = "https://bluerenting-dluj3qlvta-oa.a.run.app/api/v2/users"
+export const apiPedidos = "https://bluerenting-dluj3qlvta-oa.a.run.app/api/v2/purchases"
---
+++ src/bluerenting-pedidos/bluerenting-pedidos.js
@@ -0,0 +1,76 @@
+import { LitElement, html , css} from 'lit-element';
+import { apiPedidos } from '../api';
+import '../bluerenting-coche-edit/bluerenting-coche-edit'
+
+class BluerentingPedidos extends LitElement {
+
+    static get properties() {
+        return {
+            pedidos: {type: Array},
+        }
+    }
+
+    constructor() {
+        super()
+
+        this.pedidos = []
+        this.getPedidos()
+    }
+
+    render() {
+        return html`
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <div class="d-grid gap-2">
+            <button class="btn btn-success btn-lg" type="button" disabled>Pedidos</button>
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">User</th>
+                    <th scope="col">Importe</th>
+                    <th scope="col">Marca</th>
+                    <th scope="col">Mdelo</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+            ${this.pedidos.map(
+                pedido => html`
+                    <tr>
+                    <th scope="row">${pedido.userId}</th>
+                    <td>${pedido.totalPrice} €/mes</td>
+                    ${pedido.vehicles.map(
+                        car => html`
+                        <td>${car.name}</td>
+                        <td>${car.model}</td>
+                        <td><img src="${car.imageUrl}" heigth="50" width="50"></td>
+                    `)}
+                    </tr>
+                `)}
+            </tbody>
+        </table>
+
+        `;
+    }   
+
+    getPedidos() {
+        console.log("getPedidos")
+        let xhr = new XMLHttpRequest();
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText)
+                let APIResponse = JSON.parse(xhr.responseText);
+                this.pedidos = APIResponse
+                console.log(this.pedidos)
+                console.log("Pedidos correctamente obtenidos")
+            }
+        }
+
+        xhr.open("GET", apiPedidos);
+        xhr.send();
+    }
+
+
+}
+
+customElements.define('bluerenting-pedidos', BluerentingPedidos);
